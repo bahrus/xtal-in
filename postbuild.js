cc2e059ed2a0001876749299184cf69838da282d
bahrus <anderson.bruce.b@gmail.com>
2018-03-31 12:19:07 -0400

    .

--- postbuild.js
+++ postbuild.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 try{
-    fs.readFileSync('custom-event.js', 'utf8', function(err, data) {
+    fs.readFileSync('./custom-event.js', 'utf8', function(err, data) {
         if (err) throw err;
         console.log('OK: ');
         console.log(data)
